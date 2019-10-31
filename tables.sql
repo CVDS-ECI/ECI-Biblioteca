a531fd869e4a5725106c38e009ab5eaaf087f290
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2019-10-31 14:44:03 -0500

    Registrar Un  Recurso / Crear Tablas

--- tables.sql
+++ tables.sql
@@ -2,34 +2,38 @@ create table if not exists Rol(
 id SERIAL primary key ,
 nombre varchar(50));
 
+CREATE table if not exists  Usuario(
 
-CREATE TABLE IF NOT EXISTS Usuario(
-  correo varchar(45)NOT NULL,
-  contraseña varchar(45)not null,
+  correo varchar(50)NOT NULL,
+  passwd varchar(50)not null,
+  nombre VARCHAR(50) NOT NULL,
   rol int references Rol(id),
-  u_area varchar(45) not null,
-  nombre VARCHAR(45) NOT NULL,
+  programa varchar(50) not null,
   PRIMARY KEY (correo))
 ;
 
-
-CREATE TABLE IF NOT EXISTS Recurso (
+CREATE TABLE if not exists Recurso (
   id SERIAL,
-  nombre varchar(45) not null,
-  ubicacion varchar(45) not null,
-  tipo varchar(45) not null,
+  nombre varchar(50) not null,
+  ubicacion varchar(50) not null,
+  tipo varchar(50) not null,
   capacidad int not null,
   estado varchar(45) not null,
   PRIMARY KEY (id));
-
-
-
-CREATE TABLE IF NOT EXISTS Reserva (
-  id SERIAL,
-  tipo varchar(45) not null,
-  fecha_fin DATE,
-  periodicidad varchar(45),
-  estado varchar(45) not null,
-  usuario varchar(45) NOT NULL REFERENCES Usuario(correo),
-  recurso INTEGER NOT NULL REFERENCES Recurso(id),
-  PRIMARY KEY (id));
+ 
+ create table if not exists Horario(
+ id SERIAL,
+ hora varchar(50) not null,
+ primary KEY(id) 
+ );
+ 
+ create table if not exists Reserva (
+ id SERIAL,
+ recurso int references Recurso(id),
+ usuario varchar(50) references Usuario(correo),
+ fechainicio date not null,
+ estado char(3) not null,
+ franja_ini int references Horario(id),
+ duracion int not null
+ );
+  
