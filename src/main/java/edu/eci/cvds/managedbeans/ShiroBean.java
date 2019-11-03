8f885283ac20b45f76d265230aad3a306a81734d
Santiago <srubianof@icloud.com>
2019-11-03 16:07:49 -0500

    LogOut listo
    Para hacer log out se debe hacer iniciado sesion de lo contrario no sirve y en terminal se obtiene nullpointerexception
    
    Falta hacer que la vista recursos EXIJA obtener un elemento del shiroBean para evitar el acceso si no se la logueado

--- src/main/java/edu/eci/cvds/managedbeans/ShiroBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/ShiroBean.java
@@ -18,6 +18,7 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Level;
 import javax.faces.bean.ManagedBean;
 
 @Named
@@ -31,6 +32,8 @@ public class ShiroBean implements Serializable {
     private String username;
     private String password;
     private Boolean rememberMe = false;
+    private String redirectUrl = "/faces/login.xhtml";
+    Subject subject;
 
     public ShiroBean() {
     }
@@ -39,13 +42,12 @@ public class ShiroBean implements Serializable {
      * Try and authenticate the user
      */
     public void doLogin() {
-
-        Subject subject = SecurityUtils.getSubject();
+        subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(getUsername(), getPassword());
         try {
             subject.login(token);
             if (subject.hasRole("Administrador")) {
-                FacesContext.getCurrentInstance().getExternalContext().redirect("admin/paginas/recursos.html");
+                FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/admin/paginas/recursos.xhtml");
             } else {
                 FacesContext.getCurrentInstance().getExternalContext().redirect("newxhtml.xhtml");
             }
@@ -67,6 +69,23 @@ public class ShiroBean implements Serializable {
             token.clear();
         }
     }
+    public void doLogOut(){
+        System.err.println("1111111111111");
+//        subject=null;
+        if (subject == null) {
+            System.err.println("ayyyyyyyyyyyyyy");
+        }
+        System.err.println("222222222222");
+        subject.logout();
+        System.err.println("333333333333333333333");
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(redirectUrl);
+            System.err.println("444444444444444444");
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(ShiroBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+            
 
     /**
      * Adds a new SEVERITY_ERROR FacesMessage for the ui
@@ -99,5 +118,13 @@ public class ShiroBean implements Serializable {
 
     public void setRememberMe(Boolean lembrar) {
         this.rememberMe = lembrar;
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
     }
 }
