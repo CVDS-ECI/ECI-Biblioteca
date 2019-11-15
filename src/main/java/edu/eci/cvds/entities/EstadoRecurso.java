954bf8464320523d475116a7d8d2315e621862d4
Santiago <srubianof@icloud.com>
2019-11-14 22:39:16 -0500

    Actualizando plantilla

---
+++ src/main/java/edu/eci/cvds/entities/EstadoRecurso.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.entities;
+
+/**
+ *
+ * @author 2139687
+ */
+public enum EstadoRecurso {
+    Seleccione,Disponible,Mantenimiento,NoDisponible
+}
