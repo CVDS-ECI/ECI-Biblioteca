926b9fae9617eff563d2b55efe9df267c9ce932a
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2019-11-18 13:23:35 -0500

    Creacion de paginas de errores 404 y 401. Combobox dependientes para la reserva

--- src/main/webapp/resources/js/initiative.js
+++ src/main/webapp/resources/js/initiative.js
@@ -63,6 +63,14 @@ $(document).ready(function() {
 } );
 
 
+var $select1 = $('#select1'),
+    $select2 = $('#select2'),
+    $options = $select2.find('option');
+
+$select1.on('change', function () {
+    $select2.html($options.filter('[value="' + this.value + '"]'));
+}).trigger('change');
+
 
 
 
