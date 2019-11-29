3cf4b04afec54965050c4b42987cb46cae701ce8
david <davidcolombia9@outlook.es>
2019-11-28 21:16:37 -0500

    avance con error de bean

--- src/main/java/edu/eci/cvds/entities/Reserva.java
+++ src/main/java/edu/eci/cvds/entities/Reserva.java
@@ -22,6 +22,7 @@ public class Reserva {
     private TipoReserva tipoReserva;
     private String usuario;
     private int recurso;
+    private int cantidadDeReservas;
     
 // PILAS QUE FALTA USUARIO Y RECURSO PARA ESTO Y EL MAPPER
 
