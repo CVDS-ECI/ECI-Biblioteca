a7f9226f282efa2f21adf8c40ecc8999ddb8e348
david <davidcolombia9@outlook.es>
2019-11-29 15:12:58 -0500

    relaizar back consultas(Update mybatisReportedaoDao completo )

--- src/main/java/edu/eci/cvds/persistence/mybatisimpl/MyBatisReporteDAO.java
+++ src/main/java/edu/eci/cvds/persistence/mybatisimpl/MyBatisReporteDAO.java
@@ -23,22 +23,50 @@ public class MyBatisReporteDAO implements ReporteDAO{
 
     @Override
     public List<Reserva> recursosMasUsadosPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMasUsadosPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMasUsadosPorHora(String hora1, String hora2) throws PersistenceException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMasUsadosPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMasUsadosPorPrograma(String programa) throws PersistenceException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMasUsadosPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMasUsadosPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMasUsadosPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
@@ -55,127 +83,302 @@ public class MyBatisReporteDAO implements ReporteDAO{
 
     @Override
     public List<Reserva> recursosMenosUsadosPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMenosUsadosPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMenosUsadosPorHora(String hora1, String hora2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMenosUsadosPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMenosUsadosPorPrograma(String programa) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMenosUsadosPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMenosUsadosPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMenosUsadosPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> recursosMenosUsados() throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.recursosMenosUsados();
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMayorOcupacionPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMayorOcupacionPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMayorOcupacionPorHora(String hora1, String hora2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMayorOcupacionPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMayorOcupacionPorPrograma(String programa) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMayorOcupacionPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMayorOcupacionPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMayorOcupacionPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMayorOcupacion() throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMayorOcupacion();
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMenorOcupacionPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMenorOcupacionPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMenorOcupacionPorHora(String hora1, String hora2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMenorOcupacionPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMenorOcupacionPorPrograma(String programa) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMenorOcupacionPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMenorOcupacionPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMenorOcupacionPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> horariosMenorOcupacion() throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.horariosMenorOcupacion();
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasRecurrentesPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasRecurrentesPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasRecurrentesPorHora(String hora1, String hora2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasRecurrentesPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasRecurrentesPorPrograma(String programa) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasRecurrentesPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasRecurrentesPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasRecurrentesPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasRecurrentes() throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasRecurrentes();
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasCanceladasPorFecha(String date1, String date2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasCanceladasPorFecha(date1, date2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasCanceladasPorHora(String hora1, String hora2) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasCanceladasPorHora(hora1, hora2);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasCanceladasPorPrograma(String programa) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasCanceladasPorPrograma(programa);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasCanceladasPorTipoRecurso(String tipoRecurso) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasCanceladasPorTipoRecurso(tipoRecurso);
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
 
     @Override
     public List<Reserva> reservasCanceladas() throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+             return reporteMapper.reservasCanceladas();
+         }
+         catch(org.apache.ibatis.exceptions.PersistenceException e){
+             
+             throw new PersistenceException("Error al consultar los recursos mas usados",e);
+             
+         }	
     }
     
 }
