3180d30048837f6fc79aa3a8601e048854c899e8
david <davidcolombia9@outlook.es>
2019-11-29 20:19:16 -0500

    Realizar back consultas(finalizado)

--- src/main/java/edu/eci/cvds/managedbeans/ReporteBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/ReporteBean.java
@@ -32,16 +32,324 @@ public class ReporteBean extends BasePageBean {
 
     }
 
-    public List<Recurso> consultarRecursosMenosUsados() {
-        List<Recurso> recursos = new ArrayList<>();
+    public List<Reserva> consultarRecursosMenosUsados() {
+        List<Reserva> recursos = new ArrayList<>();
         try {
-            //recursos = serviciosBiblioteca.consultarRecursos();
+            recursos = serviciosBiblioteca.consultarRecursosMenosUsados();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return recursos;
     }
+        
+    public List<Reserva> consultarRecursosMasUsadosPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMasUsadosPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
 
+    }
+   
+    public List<Reserva> consultarRecursosMasUsadosPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMasUsadosPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarRecursosMasUsadosPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMasUsadosPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarRecursosMasUsadosPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMasUsadosPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarRecursosMenosUsadosPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMenosUsadosPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarRecursosMenosUsadosPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMenosUsadosPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarRecursosMenosUsadosPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMenosUsadosPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarRecursosMenosUsadosPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarRecursosMenosUsadosPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+        return recurso;
+
+    }
+  
+    public List<Reserva> consultarHorariosMayorOcupacionPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMayorOcupacionPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarHorariosMayorOcupacionPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMayorOcupacionPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMayorOcupacionPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMayorOcupacionPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMayorOcupacionPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMayorOcupacionPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMayorOcupacion()  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMayorOcupacion();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMenorOcupacionPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMenorOcupacionPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarHorariosMenorOcupacionPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMenorOcupacionPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMenorOcupacionPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMenorOcupacionPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarHorariosMenorOcupacionPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMenorOcupacionPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarHorariosMenorOcupacion()  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarHorariosMenorOcupacion();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasRecurrentesPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasRecurrentesPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasRecurrentesPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasRecurrentesPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasRecurrentesPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasRecurrentesPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasRecurrentesPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasRecurrentesPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasRecurrentes()  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasRecurrentes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarReservasCanceladasPorFecha(String date1, String date2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasCanceladasPorFecha(date1, date2);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+        return recurso;
+
+    }
+    
+    public List<Reserva> consultarReservasCanceladasPorHora(String hora1, String hora2)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasCanceladasPorHora(hora1, hora2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+   
+    public List<Reserva> consultarReservasCanceladasPorPrograma(String programa)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasCanceladasPorPrograma(programa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+ 
+    public List<Reserva> consultarReservasCanceladasPorTipoRecurso(String tipoRecurso)  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasCanceladasPorTipoRecurso(tipoRecurso);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+
+    public List<Reserva> consultarReservasCanceladas()  {
+        List<Reserva> recurso = new ArrayList<>();
+        try {
+            recurso = serviciosBiblioteca.consultarReservasCanceladas();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recurso;
+
+    }
+    
     public String[] getTiposDeReportes() {
         return TiposDeReportes;
     }
